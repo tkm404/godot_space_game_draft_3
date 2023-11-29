ded4d1eace5ba7b68f0710d9b47979122ea1a030
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-11-29 11:30:17 -0800

    add unique name to marker 2d to launch flares in cm script/scene

--- Scripts/CounterMeasure.gd
+++ Scripts/CounterMeasure.gd
@@ -4,6 +4,10 @@ class_name CounterMeasure
 
 @export var FLARE_SCENE: PackedScene
 
+@onready var flare_launcher = $FlareLauncher
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
